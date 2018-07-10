a2d5ebc8d29eeebd19d98b47e8c1763dfd415541
= <=>
2018-07-10 12:29:57 -0300

    finalizado css, telas home, usuários e routes

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,7 +1,13 @@
 import './Nav.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props =>
     <aside className="menu-area">
-        Navegação
+        <Link to="/">
+            <i className="fa fa-home">Home</i>
+        </Link>
+        <Link to="/users">
+            <i className="fa fa-users">Usuários</i>
+        </Link>
     </aside>
