8d4bf35b69e90f9f3ac1e5ede94a92dbdcc5ff22
Bruno Costa <brunoscosta17@gmail.com>
2018-07-21 22:34:15 -0300

    docker compose

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,46 @@
 var express = require('express');
-var app = express();
+var mongoose = require('mongoose');
 
-app.get('/', function(req, res){
-    res.send("Hello World!");
+var app = express();
+// var conString = process.env.DATABASE_URL;
+
+var conString = "mongodb://oficina5user:102938@localhost:27017/oficina5";
+
+mongoose.connect(conString, function(error) {
+    console.log(error);
+  });
+
+var userSchema = mongoose.Schema({
+    name: String,
+    email: String
+});
+
+var User = mongoose.model("User", userSchema)
+
+app.get('/user', function(req, res){
+    User.find(function(err, response){
+        if (err) { res.send("DB Error: " + err) }
+        else {
+            res.send(response);
+        }
+     });
 }); 
 
+app.post('/user', function(req, res) {
+    var newUser = new User({
+        name: "Bruno",
+        email: "brunoscosta17@gmail.com"
+    });
+
+    newUser.save(function(err, User){
+        if (err) { res.send("DB Error: " + err) }
+        else {
+            res.send("User created sucessfully.");
+        }
+    });
+
+});
+
 app.listen(3001);
+
+
